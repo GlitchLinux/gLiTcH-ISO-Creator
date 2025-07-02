8ef2a204968d5bc1e2d8c5556fe4631e70e7f5d5
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2025-07-02 19:28:25 +0200

    Update core-install.sh

--- core-install.sh
+++ core-install.sh
@@ -5,4 +5,5 @@ sudo apt update && sudo apt install -y syslinux-utils grub-efi-amd64-bin mtools 
 
 sudo rm -rf /tmp/bootfiles && sudo mkdir /tmp/bootfiles && cd /tmp/bootfiles
 sudo wget https://github.com/GlitchLinux/gLiTcH-ISO-Creator/raw/refs/heads/main/HYBRID-BASE.tar.lzma
-sudo unlzma HYBRID-BASE.tar.lzma
+sudo unlzma HYBRID-BASE.tar.lzma && sudo tar -xvf HYBRID-BASE.tar
+sudo rm HYBRID-BASE.tar && cd HYBRID-BASE
